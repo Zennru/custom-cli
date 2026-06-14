734320e9de10aa75ca4e43d6c7a6add7984ea8da
Bahtiar Rifai <bahtiarrifaii06@gmail.com>
2026-06-14 19:29:15 +0700

    Refactor exit handling in main loop and improve user feedback

--- cli.py
+++ cli.py
@@ -1,15 +1,27 @@
+def exitShell():
+    print("Leaving Jenshell. See you later!")
+
 def main():
     while True:
         try:
             user_input = input("JenShell> ")
+        except EOFError:
+            exitShell()
+            break
+        
+        if user_input.strip() == "exit":
+            exitShell()
+            break
 
-            if user_input == "exit":
-                print("Keluar dari Jenshell. Sampai jumpa!")
-                break
-
-        except KeyboardInterrupt:
-            print()  # Newline setelah ^C
+        if user_input.strip() == "":
             continue
+            
+        args = user_input.split()
+        command = args[0]
+        arguments = args[1:]
+
+        print(f"Command   : {command}")
+        print(f"Arguments : {arguments}")
 
 if __name__ == "__main__":
     main()
