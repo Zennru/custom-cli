f7c3d655d2c60b19bdcb3da32a278ce64cf9be32
Bahtiar Rifai <bahtiarrifaii06@gmail.com>
2026-06-14 19:49:02 +0700

    Refactor main loop structure for improved readability and error handling.

--- cli.py
+++ cli.py
@@ -5,23 +5,28 @@ def main():
     while True:
         try:
             user_input = input("JenShell> ")
-        except EOFError:
-            exitShell()
-            break
-        
-        if user_input.strip() == "exit":
-            exitShell()
-            break
 
-        if user_input.strip() == "":
-            continue
+            if user_input.strip() == "exit":
+                exitShell()
+                break
+
+            if user_input.strip() == "":
+                continue
+
+            args = user_input.split()
+            command = args[0]
+            arguments = args[1:]
             
-        args = user_input.split()
-        command = args[0]
-        arguments = args[1:]
+            print(f"Command   : {command}")
+            print(f"Arguments : {arguments}")
 
-        print(f"Command   : {command}")
-        print(f"Arguments : {arguments}")
+        except KeyboardInterrupt:
+            print()
+            continue
+        except EOFError:
+            print()
+            exitShell()
+            break
 
 if __name__ == "__main__":
     main()
